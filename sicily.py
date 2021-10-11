31a119a1b38820433bc7760744769588fc0ec32e
Jon Palmisciano <jp@jonpalmisc.com>
2021-10-11 19:43:48 -0400

    Ensure cursor is visible after movement

--- sicily.py
+++ sicily.py
@@ -7,4 +7,8 @@ class SicilyMoveToExtremeCommand(sublime_plugin.TextCommand):
         self.view.sel().clear()
 
         destination = 0 if location == "start" else self.view.size()
+
+        # Move the cursor to the destination, then ensure the cursor is visible
+        # after moving.
         self.view.sel().add(sublime.Region(destination))
+        self.view.show_at_center(self.view.sel()[0])
