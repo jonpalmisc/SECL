5f2d6dc50e0d01d37bfda6908674354233c79a34
Jon Palmisciano <jp@jonpalmisc.com>
2021-12-27 16:12:51 -0500

    Add hacky, fake "paragraph-based" movement (temporary)

--- secl.py
+++ secl.py
@@ -12,3 +12,11 @@ class SeclMoveToExtremeCommand(sublime_plugin.TextCommand):
         # after moving.
         self.view.sel().add(sublime.Region(destination))
         self.view.show_at_center(self.view.sel()[0])
+
+
+class SeclMoveRepeatCommand(sublime_plugin.TextCommand):
+    def run(self, edit: sublime.Edit, count: int, by: str, forward: bool, extend: bool):
+        args = {"by": by, "forward": forward, "extend": extend}
+
+        for i in range(count):
+            self.view.run_command("move", args)
