7419683a8bdabfb74e78b95459b8c0574f2a8056
Jon Palmisciano <jp@jonpalmisc.com>
2022-05-07 11:02:36 -0400

    Add proper paragraph movement

--- secl.py
+++ secl.py
@@ -20,3 +20,24 @@ class SeclMoveRepeatCommand(sublime_plugin.TextCommand):
 
         for i in range(count):
             self.view.run_command("move", args)
+
+
+class SeclMoveByParagraphCommand(sublime_plugin.TextCommand):
+    def prev_par_from(self, start: int) -> int:
+        """Get the previous paragraph from the given start point."""
+
+        pars = [p for p in self.view.find_all("\n\n", 0) if p.end() < start]
+        return 0 if len(pars) == 0 else pars[-1].end()
+
+    def next_par_from(self, start: int) -> int:
+        """Get the next paragraph from the given start point."""
+
+        next_par = self.view.find("\n\n", start).end()
+        return next_par if next_par != -1 else self.view.size()
+
+    def run(self, edit: sublime.Edit, forward: bool):
+        pos = self.view.sel()[0].begin()
+        dest = self.next_par_from(pos) if forward else self.prev_par_from(pos)
+
+        self.view.sel().clear()
+        self.view.sel().add(sublime.Region(dest))
