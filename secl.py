09e53164d12917529fb90ec55138bd9eaf5bec77
Jon Palmisciano <jp@jonpalmisc.com>
2022-05-07 11:07:52 -0400

    Clean up buffer-extreme movement

--- secl.py
+++ secl.py
@@ -3,14 +3,11 @@ import sublime_plugin
 
 
 class SeclMoveToExtremeCommand(sublime_plugin.TextCommand):
-    def run(self, edit: sublime.Edit, location: str):
+    def run(self, edit: sublime.Edit, forward: bool):
         self.view.sel().clear()
+        dest = self.view.size() if forward else 0
 
-        destination = 0 if location == "start" else self.view.size()
-
-        # Move the cursor to the destination, then ensure the cursor is visible
-        # after moving.
-        self.view.sel().add(sublime.Region(destination))
+        self.view.sel().add(sublime.Region(dest))
         self.view.show_at_center(self.view.sel()[0])
 
 
