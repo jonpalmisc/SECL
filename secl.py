3449e746b555a41f134bb921e0e511608b726557
Jon Palmisciano <jp@jonpalmisc.com>
2022-07-02 13:11:47 -0400

    Add smart commenting command

--- secl.py
+++ secl.py
@@ -2,6 +2,38 @@ import sublime
 import sublime_plugin
 
 
+class SeclComment(sublime_plugin.TextCommand):
+    def comment_start(self) -> str:
+        for v in self.view.meta_info("shellVariables", 0):
+            if v["name"] == "TM_COMMENT_START":
+                return v["value"]
+
+        return "// "
+
+    def start_comment_eol(self, edit: sublime.Edit, reg: sublime.Region) -> int:
+        """Insert a comment at the line's end, return the new point position."""
+
+        current_line = self.view.lines(reg)[0]
+        self.view.insert(edit, current_line.end(), " " + self.comment_start())
+
+        return current_line.end() + len(self.comment_start()) + 1
+
+    def run(self, edit: sublime.Edit):
+        # TODO: Support multiple regions.
+        sel = self.view.sel()
+        if len(sel) == 0:
+            return
+        reg = sel[0]
+
+        # TOOD: Jump to comment if it already exists.
+        if reg.begin() == reg.end():
+            new_point = self.start_comment_eol(edit, reg)
+            self.view.sel().clear()
+            self.view.sel().add(new_point)
+        else:
+            self.view.run_command("toggle_comment", {})
+
+
 class SeclMoveToExtremeCommand(sublime_plugin.TextCommand):
     def run(self, edit: sublime.Edit, forward: bool):
         self.view.sel().clear()
